93b849ccc7d0f246971566e04b1a72c20424c7d3
jabesv <vjabes0@gmail.com>
2022-06-02 08:49:41 -0400

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -8,15 +8,30 @@ const pokemon = getData;
 // create an instance of express
 const app = express();
 const PORT = 3000;
+//Middleware functions
+//they update the request as soon as they come in
+app.use((req, res, next) => {
+    console.log(`Running middleware function!`)
+    next(); //got to the next middleware or the response
+})
+
+//JSON Middleware
+app.use(express.json())
+//if we don't need to read data from the url.
+app.use(express.urlencoded({extend: false}))
+
+//Setup view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //Root route
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!'); 
 })
 
-//  '/pokemon' route
-app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+//  '/pokemon' route   
+app.get("/pokemon", (req, res) => {
+    res.render("index", { data: pokemon, pageTitle: "Pokemon Page" })
 })
 
 
@@ -26,3 +41,8 @@ app.get('/pokemon', (req, res) => {
 app.listen(PORT , () => {
     console.log(`Server is runninng on port: ${PORT}`);
 });
+
+
+
+
+// https://pokemondb.net/sprites
